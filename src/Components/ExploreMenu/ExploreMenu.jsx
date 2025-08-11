a4d98c93cb2d8f0b8d19fefc0ea38eb4727464a2
saugat1070 <saugatgiri1070@gmail.com>
2025-08-12 03:31:56 +0545

    explore menu complete

---
+++ src/Components/ExploreMenu/ExploreMenu.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import "./ExploreMenu.css";
+import { menu_list } from "../../assets/frontend_assets/assets";
+export default function ExploreMenu({ category, setCategory }) {
+  return (
+    <div className="explore-menu" id="explore-menu">
+      <h1>Explore our Menu</h1>
+      <p className="explore-menu-text">
+        Choose a diverse menu featuring a delectable array which is to satisfy
+        your craving and elevate your dining experience, one delicious meal at a
+        time.
+      </p>
+      <div className="explore-menu-list">
+        {menu_list.map((item, index) => {
+          return (
+            <div
+              onClick={() =>
+                setCategory((prev) =>
+                  prev === item.menu_name ? "All" : item.menu_name
+                )
+              }
+              key={index}
+              className="explore-menu-list-item"
+            >
+              <img
+                className={category === item.menu_name ? "active" : ""}
+                src={item.menu_image}
+                alt=""
+              />
+              <p>{item.menu_name}</p>
+            </div>
+          );
+        })}
+      </div>
+      <hr />
+    </div>
+  );
+}
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Home.css"
 import Header from '../../Components/Header/Header'
+import ExploreMenu from '../../Components/ExploreMenu/ExploreMenu'
 
 export default function Home() {
+
+    const [category,setCategory] = useState("All");
+
   return (
     <div>
         <Header/>
+        <ExploreMenu category={category} setCategory={setCategory} />
     </div>
   )
 }
