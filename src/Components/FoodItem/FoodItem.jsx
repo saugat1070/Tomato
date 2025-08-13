319a6e4d614aa0d0969d2cad52ebe0a20dbe34b7
saugat1070 <saugatgiri1070@gmail.com>
2025-08-14 01:04:18 +0545

    counter button done

--- src/Components/FoodItem/FoodItem.jsx
+++ src/Components/FoodItem/FoodItem.jsx
@@ -1,7 +1,34 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import { assets } from '../../assets/frontend_assets/assets'
+import "./FoodItem.css"
 export default function FoodItem({id,name,price,description,image}) {
+
+    const [itemCount,setItemCount] = useState(0)
+
   return (
-    <div>FoodItem</div>
+    <div className='food-item'>
+        <div className="food-item-img-container">
+            <img className='food-item-image' src={image} alt="" />
+            {
+                !itemCount
+                 ? <img className='add' onClick={()=>setItemCount((prev)=>prev+1)} src={assets.add_icon_white} alt=''/>
+                 : <div className='food-item-counter'>
+                    <img className='remove' onClick={()=>setItemCount((prev)=>prev-1)} src={assets.remove_icon_red} alt="" />
+                    <p className='number-count'>{itemCount}</p>
+                    <img className='add-more' onClick={()=>setItemCount((prev)=>prev+1)} src={assets.add_icon_green} alt="" />
+                 </div>
+            }
+        </div>
+        <div className="food-item-info">
+            <div className="food-item-name-rating">
+                <p>{name}</p>
+                <img src={assets.rating_starts} alt="" />
+            </div>
+            <p className="food-item-desc">
+                {description}
+            </p>
+            <p className="food-item-price">${price}</p>
+        </div>
+    </div>
   )
 }
