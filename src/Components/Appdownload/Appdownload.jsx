da372983701787f125ed9cdf8cb8ab1c90230e09
saugat1070 <saugatgiri1070@gmail.com>
2025-08-15 01:40:59 +0545

    Download app section complete

--- src/Components/Appdownload/Appdownload.jsx
+++ src/Components/Appdownload/Appdownload.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import "./Appdownload.css"
+import { assets } from '../../assets/frontend_assets/assets'
 
 
 export default function Appdownload() {
   return (
-    <div className='app-download' id=''>
-
+    <div className='app-download' id='app-download'>
+        <p>For better Experience Download <br/> Tomato App</p>
+        <div className="app-download-platform">
+            <img src={assets.play_store} alt="" />
+            <img src={assets.app_store} alt="" />
+        </div>
     </div>
   )
 }
